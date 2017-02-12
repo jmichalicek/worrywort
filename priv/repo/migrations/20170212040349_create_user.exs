f95147ff2e2d0d2dd09b41424a0b89a4fc24af7e
Justin Michalicek <jmichalicek@gmail.com>
2017-02-12 04:12:01 +0000

    initial user model

---
+++ priv/repo/migrations/20170212040349_create_user.exs
@@ -0,0 +1,18 @@
+defmodule Brewbase.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string, default: "", null: false
+      add :last_name, :string, default: "", null: false
+      add :email, :string, null: false
+      add :hashed_password, :string, default: "", null: false
+      add :is_active, :boolean, default: false, null: false
+      add :last_login, :datetime, null: true
+
+      timestamps()
+    end
+    create unique_index(:users, [:email])
+
+  end
+end
