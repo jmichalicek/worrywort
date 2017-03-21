1f61a900171fed1d6baf3c50927a1f016287f793
Justin Michalicek <jmichalicek@gmail.com>
2017-03-21 23:42:57 +0000

    properly use Guardian for auth, not the old login function

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -7,6 +7,7 @@ defmodule Brewbase.AuthController do
 
   alias Ueberauth.Strategy.Helpers
   alias Brewbase.User
+  alias Brewbase.Repo       
 
   import Comeonin.Bcrypt, only: [hashpwsalt: 1, checkpw: 2]
 
@@ -27,27 +28,20 @@ defmodule Brewbase.AuthController do
   @doc """
   Handle Ueberauth login attempt using username/password (identity strategy)
   """
-  #def callback(conn, %{"auth" => %{"email" => email, "password" => password}}) do
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do 
-    user = Brewbase.Repo.get_by(User, email: auth.uid, is_active: true)
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user = Repo.get_by(User, email: auth.uid, is_active: true)
     credentials = auth.credentials
-    # location of password seems a bit odd, but that's where it is.
-    if user && checkpw(credentials.other.password, user.hashed_password) do
+    case user && checkpw(credentials.other.password, user.hashed_password) do
+      true ->
+        conn
+        |> Guardian.Plug.sign_in(conn, user)
+        |> put_flash(:info, "Welcome, #{user.first_name}")
+        |> redirect(to: page_path(conn, :index))
+      _ ->
       conn
-      |> put_flash(:info, "Welcome, #{user.first_name}")
-      |> login(user)
-      |> redirect(to: page_path(conn, :index))
+      |> put_flash(:error, "Invalid username or password")
+      |> render("login.html")
     end
-    conn
-    |> put_flash(:error, "Invalid username or password")
-    |> render("login.html")
-  end
-
-  def login(conn, user) do
-    conn
-    |> assign(:current_user, user)
-    |> put_session(:user_id, user.id)
-    |> configure_session(renew: true)
   end
 
   @doc """
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,8 @@
 defmodule Brewbase.PageController do
   use Brewbase.Web, :controller
+  use Guardian.Phoenix.Controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  def index(conn, _params, user, claims) do
+    render(conn, "index.html", current_user: user)
   end
 end
